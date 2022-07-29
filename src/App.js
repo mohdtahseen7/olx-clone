28f6e752324b9486b91381d7ef54b32d357d5ed5
unknown <tahseenmohd1111@gmail.com>
2022-07-29 18:08:51 +0530

    Signup Page

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router ,Route} from "react-router-dom";
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Router>
+     <Route exact path='/'>
+          <Home />
+     </Route>
+      <Route  path='/Login'>
+          <Login/>
+      </Route>
+      <Route  path='/signup'>
+          <Signup/>
+      </Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
